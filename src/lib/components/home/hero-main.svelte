fab3e2815b45247bda951eec4fde88b450834a85
Rodrigo Santos da Silva <rodsantosdasilva@gmail.com>
2024-06-01 12:22:39 +0000

    Adding main hero

---
+++ src/lib/components/home/hero-main.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { Button } from '../ui/button';
+</script>
+
+<div class="relative overflow-hidden py-24 lg:py-32">
+	<div aria-hidden="true" class="flex absolute -top-96 start-1/2 transform -translate-x-1/2">
+		<div
+			class="bg-gradient-to-r from-background/50 to-background blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]"
+		/>
+		<div
+			class="bg-gradient-to-tl blur-3xl w-[90rem] h-[50rem] rounded-full origin-top-left -rotate-12 -translate-x-[15rem] from-primary-foreground via-primary-foreground to-background"
+		/>
+	</div>
+	<div class="relative z-10">
+		<div class="container py-10 lg:py-16">
+			<div class="max-w-2xl text-center mx-auto">
+				<p class="">Elevate your projects</p>
+				<div class="mt-5 max-w-2xl">
+					<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+						Beautiful UI Blocks
+					</h1>
+				</div>
+				<div class="mt-5 max-w-3xl">
+					<p class="text-xl text-muted-foreground">
+						Over 10+ fully responsive, UI blocks you can drop into your Shadcn UI projects and
+						customize to your heart&apos;s content.
+					</p>
+				</div>
+				<div class="mt-8 gap-3 flex justify-center">
+					<Button size="lg">Get started</Button>
+					<Button size="lg" variant="outline">Learn more</Button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import HeroMain from '@/lib/components/home/hero-main.svelte';
 	import HeroCaptureEmail from '@/lib/components/home/hero-capture-email.svelte';
 </script>
 
+<HeroMain />
 <HeroCaptureEmail />
